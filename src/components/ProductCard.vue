1590a2f182562bc0c77bce2c52bdccf84054093e
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-04-24 17:45:38 +0200

    feat: :sparkles: add layout and product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class=""></section>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const Button = defineAsyncComponent(() => import("./Button.vue"));
+
+interface Product {
+  imageSrc: string;
+  imageAlt: string;
+  href: string;
+  name: string;
+  color: string;
+  price: number;
+  currency: string;
+}
+</script>
