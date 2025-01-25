bf1405fe01aae0e753d404d791d162fd0fdb51ac
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2025-01-25 15:48:22 +0200

    feat: :sparkles: Enhance cart with product details and quantity management

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,32 +1,110 @@
 <template>
   <main class="p-3">
-    <header
-      class="p-3 flex-between-center gap-3 border-bottom-1 border-gray-300">
-      <h2 class="text-2xl font-semibold text-gray-900">cart</h2>
-      <Button
-        icon="pi pi-trash"
-        label="Clear"
-        size="small"
-        severity="secondary" />
+    <header class="p-3 border-bottom-1 border-gray-300">
+      <h1 class="text-2xl font-semibold text-gray-900">Cart</h1>
     </header>
 
-    <ul>
-      <li v-for="item in cartStore.cart" :key="item.id">
-        {{ item.name }} - {{ item.quantity }} x {{ item.currency
-        }}{{ item.price }}
-        <button @click="cartStore.removeFromCart(item.id)">Remove</button>
-      </li>
-    </ul>
-    <p>Total Items: {{ cartStore.totalItems }}</p>
-    <p>Total Price: {{ cartStore.totalPrice }}</p>
-    <button @click="cartStore.clearCart()">Clear Cart</button>
+    <section
+      v-if="cartStore.cart.length > 0"
+      class="products-list p-3 flex flex-column gap-3">
+      <div
+        v-for="item in cartStore.cart"
+        :key="item.id"
+        class="product flex-start-center gap-3 border-bottom-1 border-gray-300">
+        <div
+          class="product-image border-1 border-bottom-none border-gray-300 border-round-top-lg overflow-hidden">
+          <img :src="item.image" :alt="item.name" width="180" />
+        </div>
+        <div class="product-details flex-grow-1 flex-between-start gap-3">
+          <div class="flex flex-column gap-3">
+            <span class="text-lg font-medium text-gray-900">
+              {{ item.name }}
+            </span>
+            <span class="text-base font-medium text-gray-700">
+              {{ item.currency + item.price }} |
+              <span class="text-green-500">In Stock</span>
+            </span>
+
+            <div class="mt-3 flex-start-center gap-2 w-fit">
+              <Button
+                icon="pi pi-minus"
+                severity="secondary"
+                @click="decreaseQuantity(item.id)" />
+
+              <InputNumber
+                class="max-w-5rem border-white"
+                v-model="item.quantity"
+                :min="1"
+                fluid />
+
+              <Button
+                icon="pi pi-plus"
+                severity="secondary"
+                @click="increaseQuantity(item.id)" />
+            </div>
+          </div>
+
+          <div class="flex flex-column gap-3">
+            <span class="text-xl font-semibold text-gray-900">
+              {{ item.currency + (item.price * item.quantity).toFixed(2) }}
+            </span>
+            <Button
+              label="Remove"
+              icon="pi pi-trash"
+              size="small"
+              severity="danger"
+              @click="cartStore.removeFromCart(item.id)" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer
+      v-if="cartStore.cart.length > 0"
+      class="p-3 border-top-1 border-gray-300">
+      <div class="flex-between-start">
+        <div class="flex flex-column gap-3">
+          <span class="text-xl font-semibold text-gray-900">
+            Total Items: {{ cartStore.totalItems }}
+          </span>
+          <span class="text-xl font-semibold text-gray-900">
+            Total Price: {{ cartStore.totalPrice }}
+          </span>
+        </div>
+
+        <Button
+          label="Clear Cart"
+          icon="pi pi-trash"
+          size="small"
+          severity="danger"
+          @click="cartStore.clearCart()" />
+      </div>
+    </footer>
+
+    <div v-else class="flex-center h-10rem">
+      <span class="text-lg font-medium text-gray-700">
+        Your cart is empty
+      </span>
+    </div>
   </main>
 </template>
 
 <script setup>
   // PrimeVue Components
   import Button from "primevue/button";
+  import InputNumber from "primevue/inputnumber";
   // Cart store
   import { useCartStore } from "../store/cart";
   const cartStore = useCartStore();
+
+  // Quantity logic
+  const increaseQuantity = (productId) => {
+    const item = cartStore.cart.find((p) => p.id === productId);
+    if (item) item.quantity++;
+  };
+
+  const decreaseQuantity = (productId) => {
+    const item = cartStore.cart.find((p) => p.id === productId);
+    if (item && item.quantity > 1) item.quantity--;
+  };
 </script>
